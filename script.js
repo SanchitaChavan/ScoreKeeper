b40bf9bf043aad3edd18bc16decead9bc38be453
SanchitaChavan <60959615+SanchitaChavan@users.noreply.github.com>
2020-06-09 17:44:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+var p1Button = document.querySelector("#p1");
+var p2Button = document.querySelector("#p2");
+var p1Display = document.querySelector("#p1Display");
+var p1Display = document.querySelector("#p1Display");
+var resetB = document.querySelector("#reset");
+var numInput = document.querySelector("input");
+var p = document.querySelector("p");
+winningScoreDisplay = document.querySelector("p span");
+var p1Score = 0;
+var p2Score = 0;
+var gameOver = false;
+var winningScore= 5;
+
+p1Button.addEventListener("click",function(){
+    if(!gameOver){	
+	p1Score++;
+	if(p1Score ===winningScore){
+		p1Display.classList.add("winner");
+		gameOver=true;
+	}
+    p1Display.textContent=p1Score;
+}
+});
+
+p2Button.addEventListener("click",function(){
+    if(!gameOver){	
+	p2Score++;
+	if(p2Score ===winningScore){
+		p2Display.classList.add("winner");
+		gameOver=true;
+	}
+    p2Display.textContent=p2Score;
+}
+});
+numInput.addEventListener("change",function(){
+	winningScoreDisplay.textContent = numInput.value;
+	winningScore = Number(numInput.value); 
+	reset();   
+})
+resetB.addEventListener("click",function(){
+    reset();
+});
+
+function reset(){
+	gameOver= false;
+    p1Display.textContent=0;
+    p2Display.textContent=0;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.classList.remove("winner");
+    p2Display.classList.remove("winner");
+}
